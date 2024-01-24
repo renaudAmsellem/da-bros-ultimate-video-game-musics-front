d29975e334238e5c0ef2343d902ea6bb4493a87f
Renaud Amsellem <amsellem.renaud@gmail.com>
2024-01-24 02:04:54 +0100

    progress bar, stars

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <script setup>
 import { ref } from 'vue';
-import AudioPlayer from './components/AudioPlayer.vue';
 import musicNames from './assets/musicNames?raw'
+import Stars from './components/Stars.vue';
+import ProgressBar from './components/ProgressBar.vue';
 
 let audio;
+const progress = ref(0)
+const duration = ref(0)
 const isPaused = ref(true);
 const fileName = ref('')
 
 const selectFileToPlay = () => {
-  const files = musicNames.split(',')
+  const files = musicNames.split('.mp3,')
   let max = files.length - 1;
   let min = 0;
   
   let index = Math.round(Math.random() * (max - min) + min);
   let file = files[index];
-  fileName.value = file.substring(0, file.indexOf('.mp3'))
+  fileName.value = file
   return file
 }
 
 const selectNewFile = () => {
   const fileToPlay = selectFileToPlay()
-  audio = new Audio(fileToPlay)
-  audio.addEventListener("ended", () => console.log('ended') || next());
-  console.log(audio)
+  audio = new Audio(fileToPlay + '.mp3')
+  audio.addEventListener("ended", next);
+  audio.addEventListener("timeupdate", () =>  progress.value = audio.currentTime);
+  audio.addEventListener("loadedmetadata", () =>  duration.value = audio.duration);
 }
 
 const play = async() => {
@@ -46,12 +50,96 @@ selectNewFile()
 </script>
 
 <template>
-  <AudioPlayer :file-name="fileName" :play="play" :pause="pause" :next="next" :is-paused="isPaused" />
+  <div class="player__container">
+    <div class="mario_image">
+      <img src="./assets/mario_listening_music.png" alt="Album cover" />
+    </div>
+    <div class="body__info">
+      <div class="info__song">{{ fileName }}</div>
+    </div>
+
+    <div>
+      <ProgressBar :progress="progress" :duration="duration"/>
+    </div>
+
+    <div class="body__buttons">
+      <ul class="list list--buttons">
+        <li><a href="#" class="list__link"><i class="fa fa-step-backward"></i></a></li>
+
+        <li v-show="isPaused" @click="play"><img src="./assets/playIcon.png" /></li>
+        <li v-show="!isPaused" @click="pause"><img src="./assets/pauseIcon.png" /></li>
+
+        <li @click="next"><img src="./assets/nextIcon.png" /></li>
+      </ul>
+    </div>
+    <Stars class="stars" :fileName="fileName" />
+  </div>
 </template>
 
-<style scoped>
-button {
-  margin: 10px;
-  background-color: rgb(237, 98, 98);
+<style lang="scss">
+html {
+  font-size: 16px;
+}
+
+body {
+  font-family: 'Roboto', Arial, Verdana, sans-serif;
+  background: #e4f2fb;
+}
+
+.player__container {
+  height: calc(100vh - 64px);
+  max-height: calc(100vh - 64px);
+  width: 720px;
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 10px 20px -5px rgba(0, 0, 0, .19), 0 6px 6px -10px rgba(0, 0, 0, .23);;
+}
+
+.mario_image {
+  max-height: 100%;
+  height: 60%;
+}
+
+img {
+  height: 100%;
+}
+
+.list {
+  display: flex;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+.body__info {
+  padding-top: 1rem;
+  padding-bottom: 1em;
+}
+
+.info__song {
+  font-size: 1.5rem;
+  color: #d30320;
+}
+
+.list--buttons {
+  align-items: center;
+  justify-content: center;
+}
+
+.list--buttons li:nth-of-type(n+1) {
+  margin-left: 1.5rem;
+}
+
+li {
+    cursor: pointer;
+}
+
+.stars {
+  padding-bottom: 1em;
 }
 </style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { watch } from 'vue';
+
+const props = defineProps(['progress', 'duration'])
+
+watch(() => props.progress, () => {
+    const progressbar = document.getElementById('progress-bar');
+    progressbar.value = (props.progress / props.duration);
+    console.log(progressbar.value)
+})
+</script>
+
+<template>
+    <progress id="progress-bar" value="0" max="1"></progress>
+</template>
+
+<style>
+progress {
+    width: 90%;
+    margin-bottom: 1em;
+}
+
+progress[value] {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border: 1px solid black;
+    border-radius: 8px;
+    background-color: white;
+    color: #d30320;
+    height: 10px;
+}
+
+progress[value]::-webkit-progress-bar {
+    background-color: white;
+    border-radius: 2px;
+    color: #d30320;
+}
+
+progress[value]::-moz-progress-bar {
+  background-color: #d30320;
+}
+
+progress::-webkit-progress-value {
+    background-color: #d30320;
+}
+</style>
